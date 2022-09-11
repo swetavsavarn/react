5004076c60751c81f20839701090a30cb63c555f
swetavsavarn <swetavsavarn@gmail.com>
2022-09-11 21:05:32 +0530

    react

---
+++ 01-starting-setup/src/components/ExpenseItem.js
@@ -0,0 +1,13 @@
+function ExpenseItem() {
+    return <div>
+        <div>march 29th
+            <div>
+                <h2>$200</h2>
+                <div>Car insurance</div>
+            </div>
+        </div>
+    </div>
+
+
+}
+export default ExpenseItem
